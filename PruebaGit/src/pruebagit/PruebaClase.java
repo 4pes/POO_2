fc0859057699e71edc6aaa58372db7b218039a4b
billions69 <p.piero@hotmail.com>
2017-11-02 14:54:58 -0300

    Se crea uan clase y un print

---
+++ PruebaGit/src/pruebagit/PruebaClase.java
@@ -0,0 +1,29 @@
+package pruebagit;
+
+public class PruebaClase {
+    int id;
+    String nombre;
+
+    public PruebaClase(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+}
